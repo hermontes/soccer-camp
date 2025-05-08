d679e9678004d8ce8937443d3d5a2335dd2fd8b2
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-05-08 08:24:04 -0400

    Authentication logic

---
+++ src/app/utils/authentication/users-auth.js
@@ -0,0 +1,51 @@
+"use server";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+
+export const signUserIn = async (data) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  //if they attempt to sign in while there's already a session in this browser, we forbid it and force them to the dashboard
+  if (!session) {
+    return await auth.api.signInEmail({
+      body: {
+        email: data.email,
+        password: data.password,
+        rememberMe: false
+      },
+    });
+  } else {
+    // console.log((await session).user.name)
+    throw redirect("/dashboard");
+  }
+};
+
+export const signUpUser = async (data) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  //if they attempt to sign up while there's already a session in this browser, we forbid it and force them to the dashboard
+  if (!session) {
+    const response = await auth.api.signUpEmail({
+      body: {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      },
+    });
+
+    return response;
+    // const rtnResponse = {
+    //   statusCode: 200,
+
+    // }
+    // return rtnResponse;
+  } else {
+    // console.log((await session).user.name)
+    throw redirect("/dashboard");
+  }
+};
