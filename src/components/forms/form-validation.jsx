9d044f82b3549802c1d4754910c63df9131f1bf8
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-05-08 08:24:50 -0400

    Validation component helpers

---
+++ src/components/forms/form-validation.jsx
@@ -0,0 +1,41 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamation } from "@fortawesome/free-solid-svg-icons";
+
+function SubmitButton({ isSubmitting, pendingMessage, defaultMessage }) {
+  return (
+    <button
+      disabled={isSubmitting}
+      type="submit"
+      className={`flex w-full justify-center rounded-md  px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${
+        isSubmitting
+          ? `bg-indigo-300`
+          : `bg-indigo-600 hover:bg-indigo-500 cursor-pointer`
+      } `}
+    >
+      {isSubmitting ? pendingMessage : defaultMessage}
+    </button>
+  );
+}
+
+function DisplayErrorMessage({ message }) {
+  return (
+
+    <div className="flex flex-col text-[#B3251E] text-xs">
+    <div className="flex items-center ml-1">
+      {/* <div className="w-3 h-3 ml-1 mr-1 rounded-full bg-[#B3251E] flex items-center justify-center">
+        <FontAwesomeIcon
+          icon={faExclamation}
+          className="text-[12px] text-white "
+        />
+      </div> */}
+      <span>{message.split(/(?= -)/)[0]}</span>
+    </div>
+    <div className="ml-4">
+      {message.split(/(?= -)/).slice(1).map((part, index) => (
+        <span key={index} className="block"> {part}</span>
+      ))}
+    </div>
+  </div>
+  );
+}
+export {SubmitButton, DisplayErrorMessage};
