1d32157fdad922e119cdbe666e2b692c9592aabe
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-05-08 08:25:45 -0400

    Middleware

---
+++ src/middleware.js
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+//Checks session when routing between pages
+export async function middleware(request) {
+  const sessionCookie = getSessionCookie(request);
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/blog", "/dashboard"], // Specify the routes the middleware applies to
+};
+// For later: The getSessionCookie() function does not automatically reference the auth config specified in auth.ts. Therefore, you need to ensure that the configuration in getSessionCookie() matches the config defined in your auth.ts.
+
+
