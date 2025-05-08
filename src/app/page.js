c2a81a4c3aac1120152f0727fe652eba2e9f1231
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-05-08 08:24:34 -0400

    Home landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,10 @@
-"use client"
-import Navigation from "@/components/ui/Navigation";
+"use client";
+import Navigation from "@/components/ui/navigation/Navigation";
 
 import { useSession } from "@/lib/auth-client";
 
 export default function HomePage() {
   const { data: session } = useSession();
 
-  return (
-    <div className="flex w-full bg-center">
-      HomePage content
-    </div>
-  );
+  return <div className="flex w-full bg-center">HomePage content</div>;
 }
