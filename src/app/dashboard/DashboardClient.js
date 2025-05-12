16213730cc2df2c011e9de70dfda63ee29ebf043
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-05-12 09:19:57 -0400

    Add grid column dashboard with user info and countdown to camp start

--- src/app/dashboard/DashboardClient.js
+++ src/app/dashboard/DashboardClient.js
@@ -7,15 +7,16 @@ import { useEffect, useState } from "react";
 import DynamicComponent from "./DynamicComponent";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Badge } from "@/components/ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import { Clock } from "lucide-react";
 import { Progress } from "@/components/ui/progress";
 
-
 export default function DashboardClient({ user }) {
   const [newName, setNewName] = useState("");
+  const [newImage, setNewImage] = useState("");
+
   const {
     data: session,
     isPending,
@@ -26,7 +27,7 @@ export default function DashboardClient({ user }) {
 
   const updateUser = async () => {
     await authClient.updateUser({
-      image: "https://example.com/image.jpg",
+      image: newImage,
       name: newName,
     });
   };
@@ -37,30 +38,13 @@ export default function DashboardClient({ user }) {
 
   const router = useRouter();
 
-  // useEffect(() => {
-  //   if (session) {
-  //     console.log("Session updated:", session);
-  //     // if (!session) {
-  //     refetch()
-  //     // router.push("/signup");
-  //   }
-  //   // }
-  // }, [session, isPending]);
-
   const listSessions = async () => {
-    // try {
-    //   const sessions = await authClient.listSessions();
-    //   console.log(sessions);
-    // } catch (err) {
-    //   console.log("Error printing sessions");
-    // }
+
 
     try {
-      // const revoked = await authClient.revokeSessions();
-      // console.log("signed out successfully: ", revoked);
-      // const list = await authClient.listSessions();
 
-      const signOut = await authClient.signOut();
+
+      const w = await authClient.signOut();
 
       // const sessions = await authClient.listSessions();
       // console.log(list);
@@ -72,65 +56,77 @@ export default function DashboardClient({ user }) {
 
   return (
     <>
-    
-      <Card className="m-10">
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            Registration Status
-          </CardTitle>
-          <Badge
-            variant={
-              session?.user ? "default" : "outline"
-            }
-            className="bg-[#4CAF50]"
-          >
-            {session?.user.emailVerified}
-          </Badge>
-        </CardHeader>
-        <CardContent>
-          <div className="flex items-center space-x-4">
-            <Avatar>
-              <AvatarImage
-                src={user.avatar || "/placeholder.svg"}
-                alt={user.name}
-              />
-              <AvatarFallback>{session?.user.name.charAt(0)}</AvatarFallback>
-            </Avatar>
-            <div>
-              <p className="text-sm font-medium leading-none">{session?.user.name}</p>
-              <p className="text-sm text-muted-foreground">{session?.user.email}</p>
+      <div className="grid md:grid-cols-2 lg:grid-cols-2  p-5 sm:p-20 gap-2 grid-cols-1">
+        <Card className="">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Registration Status
+            </CardTitle>
+            <Badge
+              variant={session?.user ? "default" : "outline"}
+              className="bg-[#4CAF50]"
+            >
+              Confirmed
+            </Badge>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center space-x-4">
+              <Avatar>
+                <AvatarImage className=""
+                  src={session?.user.image || "/placeholder.svg"}
+                  alt={session?.user.name}
+                />
+                <AvatarFallback className="bg-muted">{session?.user.name.charAt(0)}</AvatarFallback>
+              </Avatar>
+              <div>
+                <p className="text-sm font-medium leading-none">
+                  {session?.user.name}
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  {session?.user.email}
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="mt-4 space-y-2">
-            <div className="flex items-center justify-between text-sm">
-              <span className="text-muted-foreground">Payment Status:</span>
-              <span className="font-medium">{session?.user.paymentStatus}</span>
+            <div className="mt-4 space-y-2">
+              <div className="flex items-center justify-between text-sm">
+                <span className="text-muted-foreground">
+                  Email Varification:
+                </span>
+                <span className="font-medium">
+                  {session?.user.emailVerified === false ? (
+                    <Badge className="bg-amber-700">Not Varified</Badge>
+                  ) : (
+                    <Badge className="bg-[#4CAF50]">Varified</Badge>
+                  )}
+                </span>
+              </div>
+              <div className="flex items-center justify-between text-sm">
+                <span className="text-muted-foreground">Registration ID:</span>
+                <span className="font-medium">{session?.user.id.slice(0,6)}</span>
+              </div>
             </div>
-            <div className="flex items-center justify-between text-sm">
-              <span className="text-muted-foreground">Registration ID:</span>
-              <span className="font-medium">{session?.user.id}</span>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Camp Countdown
+            </CardTitle>
+            <Clock className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">45 days</div>
+            <p className="text-xs text-muted-foreground">
+              Days until camp starts
+            </p>
+            <Progress value={((100 - 45) * 90) / 100} className="mt-3" />
+            <div className="mt-2 text-xs text-muted-foreground">
+              Camp starts on <span className="font-medium">July 8, 2025</span>
             </div>
-          </div>
-        </CardContent>
-      </Card>
-
-
-      <Card className="m-10">
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Camp Countdown</CardTitle>
-          <Clock className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">45 days</div>
-          <p className="text-xs text-muted-foreground">Days until camp starts</p>
-          <Progress value={(100-45)*(90) / 100} className="mt-3" />
-          <div className="mt-2 text-xs text-muted-foreground">
-            Camp starts on <span className="font-medium">July 8, 2025</span>
-          </div>
-        </CardContent>
-      </Card>
-
-
+          </CardContent>
+        </Card>
+      </div>
       <div className="flex w-full flex-col items-center p-[10rem]">
         <div className="">Current session user name: {user.name}</div>
         <div>Current session user email: {user.email}</div>
@@ -147,6 +143,12 @@ export default function DashboardClient({ user }) {
           value={newName}
           onChange={(e) => setNewName(e.target.value)}
           placeholder="Set new name..."
+        />
+        <input className="outline-none"
+          type="name"
+          value={newImage}
+          onChange={(e) => setNewImage(e.target.value)}
+          placeholder="Set new image..."
         />
         <button
           className="cursor-grab bg-cyan-500 p-[1rem]"
