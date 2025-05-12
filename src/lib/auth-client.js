5edab1b0a9c00f0ef0e57f383643affb71ab718c
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-05-12 15:36:38 -0400

    Add better auth base url in auth-client

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,9 +1,9 @@
 // This gives us tools for the frontend to interact with the auth system.
-// Like telling Better-Auth we want to login which then receives those api requests(routes.js), follows rules in auth.js and gets processes in Better-Auth
+// Like telling Better-Auth we want to login which then receives those api requests(routes.js), follows rules in auth.js and gets processed in Better-Auth
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.BETTER_AUTH_URL
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient
