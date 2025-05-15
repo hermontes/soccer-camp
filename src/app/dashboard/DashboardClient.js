7406600a90d0b807a220ef4de270ac2767bfd1f9
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-05-15 07:24:39 -0400

    Preparing dashboard to display more info related to the current session

--- src/app/dashboard/DashboardClient.js
+++ src/app/dashboard/DashboardClient.js
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -10,7 +9,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-import { Clock } from "lucide-react";
+import { Clock, Users } from "lucide-react";
 import { Progress } from "@/components/ui/progress";
 import { createContext } from "react";
 export const NavContext = createContext(1);
@@ -20,7 +19,6 @@ export default function DashboardClient({ user }) {
   const [newImage, setNewImage] = useState("");
 
   const router = useRouter();
-
 
   const updateUser = async () => {
     try {
@@ -64,73 +62,157 @@ export default function DashboardClient({ user }) {
 
   return (
     <>
-      <div className="grid md:grid-cols-2 lg:grid-cols-2  p-5 sm:p-20 gap-2 grid-cols-1">
-        <Card className="">
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Registration Status
-            </CardTitle>
-            <Badge
-              variant={user ? "default" : "outline"}
-              className="bg-[#4CAF50]"
-            >
-              Confirmed
-            </Badge>
-          </CardHeader>
-          <CardContent>
-            <div className="flex items-center space-x-4">
-              <Avatar>
-                <AvatarImage className="" src={user.image} alt={user.name} />
-                <AvatarFallback className="bg-muted">
-                  {user.name.charAt(0)}
-                </AvatarFallback>
-              </Avatar>
-              <div>
-                <p className="text-sm font-medium leading-none">{user.name}</p>
-                <p className="text-sm text-muted-foreground">{user.email}</p>
+      <div className="flex min-h-screen flex-col">
+        <main className="flex flex-1 flex-col bg-muted/40">
+          <div className="w-full mx-auto flex-1 space-y-4 p-8 pt-6">
+            <div className="flex-1 space-y-4">
+              <div className="flex items-center justify-between px-2">
+                <div className="grid gap-1">
+                  <h1 className="text-3xl font-bold tracking-tight">
+                    Dashboard
+                  </h1>
+                  <p className="text-muted-foreground">
+                    Welcome back! Manage your soccer camp registration and
+                    information.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="mt-4 space-y-2">
-              <div className="flex items-center justify-between text-sm">
-                <span className="text-muted-foreground">
-                  Email Verification:
-                </span>
-                <span className="font-medium">
-                  {user.emailVerified === false ? (
-                    <Badge className="bg-amber-700">Not Verified</Badge>
-                  ) : (
-                    <Badge className="bg-[#4CAF50]">Verified</Badge>
-                  )}
-                </span>
-              </div>
-              <div className="flex items-center justify-between text-sm">
-                <span className="text-muted-foreground">Registration ID:</span>
-                <span className="font-medium">{user.id.slice(0, 6)}</span>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
+              <div className="grid gap-6">
+                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                  <Card className="rounded-lg border bg-card text-card-foreground shadow-sm">
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Registration Status
+                      </CardTitle>
+                      <Badge
+                        variant={user ? "default" : "outline"}
+                        className="bg-[#4CAF50]"
+                      >
+                        Confirmed
+                      </Badge>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="flex items-center space-x-4">
+                        <Avatar>
+                          <AvatarImage
+                            className=""
+                            src={user.image}
+                            alt={user.name}
+                          />
+                          <AvatarFallback className="bg-muted">
+                            {user.name.charAt(0)}
+                          </AvatarFallback>
+                        </Avatar>
+                        <div>
+                          <p className="text-sm font-medium leading-none">
+                            {user.name}
+                          </p>
+                          <p className="text-sm text-muted-foreground">
+                            {user.email}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="mt-4 space-y-2">
+                        <div className="flex items-center justify-between text-sm">
+                          <span className="text-muted-foreground">
+                            Email Verification:
+                          </span>
+                          <span className="font-medium">
+                            {user.emailVerified === false ? (
+                              <Badge className="bg-amber-700">
+                                Not Verified
+                              </Badge>
+                            ) : (
+                              <Badge className="bg-[#4CAF50]">Verified</Badge>
+                            )}
+                          </span>
+                        </div>
+                        <div className="flex items-center justify-between text-sm">
+                          <span className="text-muted-foreground">
+                            Registration ID:
+                          </span>
+                          <span className="font-medium">
+                            {user.id.slice(0, 6)}
+                          </span>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
 
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Camp Countdown
-            </CardTitle>
-            <Clock className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">45 days</div>
-            <p className="text-xs text-muted-foreground">
-              Days until camp starts
-            </p>
-            <Progress value={((100 - 45) * 90) / 100} className="mt-3" />
-            <div className="mt-2 text-xs text-muted-foreground">
-              Camp starts on <span className="font-medium">July 8, 2025</span>
-            </div>
-          </CardContent>
-        </Card>
-      </div>
-      {/* <div className="flex w-full flex-col items-center p-[10rem]">
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Camp Countdown
+                      </CardTitle>
+                      <Clock className="h-4 w-4 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">45 days</div>
+                      <p className="text-xs text-muted-foreground">
+                        Days until camp starts
+                      </p>
+                      <Progress
+                        value={((100 - 45) * 90) / 100}
+                        className="mt-3"
+                      />
+                      <div className="mt-2 text-xs text-muted-foreground">
+                        Camp starts on{" "}
+                        <span className="font-medium">July 8, 2025</span>
+                      </div>
+                    </CardContent>
+                  </Card>
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Camp Countdown
+                      </CardTitle>
+                      <Clock className="h-4 w-4 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">45 days</div>
+                      <p className="text-xs text-muted-foreground">
+                        Days until camp starts
+                      </p>
+                      <Progress
+                        value={((100 - 45) * 90) / 100}
+                        className="mt-3"
+                      />
+                      <div className="mt-2 text-xs text-muted-foreground">
+                        Camp starts on{" "}
+                        <span className="font-medium">July 8, 2025</span>
+                      </div>
+                    </CardContent>
+                  </Card>
+
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Team Information
+                      </CardTitle>
+                      <Users className="h-4 w-4 text-muted-foreground" />
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">Hawker B</div>
+                      <p className="text-xs text-muted-foreground">
+                        Age Group: 10-12 years
+                      </p>
+                      <div className="mt-4 space-y-2">
+                        <div className="flex items-center justify-between text-sm">
+                          <span className="text-muted-foreground">Coach:</span>
+                          <span className="font-medium">Michael Johnson</span>
+                        </div>
+                        <div className="flex items-center justify-between text-sm">
+                          <span className="text-muted-foreground">
+                            Team Size:
+                          </span>
+                          <span className="font-medium">12 players</span>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              </div>
+              {/* <div className="flex w-full flex-col items-center p-[10rem]">
         <div className="">Current session user name: {user.name}</div>
         <div>Current session user email: {user.email}</div>
         <div>Current session user ID: {user.id}</div>{" "}
@@ -161,6 +243,10 @@ export default function DashboardClient({ user }) {
           Update User
         </button>
       </div> */}
+            </div>
+          </div>
+        </main>
+      </div>
     </>
   );
 }
