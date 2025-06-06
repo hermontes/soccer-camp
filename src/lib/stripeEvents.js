8aa3379002b3ef35bae724920f76ddf820e57329
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-06-06 10:14:08 -0400

    create a list of allowed events

---
+++ src/lib/stripeEvents.js
@@ -0,0 +1,12 @@
+export const allowedEvents = [
+  "checkout.session.completed",
+  "invoice.paid",
+  "invoice.payment_failed",
+  "invoice.payment_action_required",
+  "invoice.upcoming",
+  "invoice.marked_uncollectible",
+  "invoice.payment_succeeded",
+  "payment_intent.succeeded",
+  "payment_intent.payment_failed",
+  "payment_intent.canceled",
+];
