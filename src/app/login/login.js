7df3d4dc154dcb862335fd12f6d776921d9eb58e
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-06-23 14:31:02 -0400

    Remove comments

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -91,11 +91,7 @@ export default function LogInPage() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          {/* <img
-            alt="Your Company"
-            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
-          /> */}
+
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
             Sign in to your account
           </h2>
@@ -104,7 +100,6 @@ export default function LogInPage() {
         <div className="!shadow-md w-full max-w-md mt-10 sm:mx-auto sm:w-full sm:max-w-sm md:max-w-md bg-[#FFFFFF] p-[3rem] rounded-md ">
           <form
             onSubmit={handleSubmit((data) => handleLogIn(data))}
-            // onSubmit={handleSubmit((data) => sendFormToServer(data))}
             method="POST"
             className="space-y-6 "
           >
