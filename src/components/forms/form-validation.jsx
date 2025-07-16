6d01a0561612cbb990f5151dfaf85e6903979c32
Hermon Tesfatsion <64483255+hermontes@users.noreply.github.com>
2025-07-16 10:11:17 -0400

    Remove unused imports

--- src/components/forms/form-validation.jsx
+++ src/components/forms/form-validation.jsx
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExclamation } from "@fortawesome/free-solid-svg-icons";
 import { Loader2 } from "lucide-react";
 
 function SubmitButton({ isSubmitting, defaultMessage }) {
